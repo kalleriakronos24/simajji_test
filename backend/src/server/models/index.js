0a45700197a1f86e435321094b8e5966d3534cb8
kalleriakronos24 <kalleriakronos24@gmail.com>
2022-08-11 19:04:49 +0800

    added sign in, sign up api

---
+++ backend/src/server/models/index.js
@@ -0,0 +1,26 @@
+import database from '../../core/models/index';
+
+
+class Model {
+
+    /**
+     * @description Cart | Cart Model
+     */
+    cart() {
+        return database.Cart
+    }
+
+    /**
+     * @description Users | User Model
+     */
+    user() {
+        return database.User
+    }
+
+    auth() {
+        return database.Auth
+    }
+
+}
+
+export default Model;
